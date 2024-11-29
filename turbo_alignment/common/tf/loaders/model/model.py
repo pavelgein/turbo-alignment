6998ce18f1acc0bf3dffa33e6240629fb62120e7
Pavel Geyn <p.geyn@tinkoff.ru>
2024-11-29 11:08:01 +0500

    looks like first working version

--- turbo_alignment/common/tf/loaders/model/model.py
+++ turbo_alignment/common/tf/loaders/model/model.py
@@ -50,15 +50,6 @@ def load_model(
     model_settings: PreTrainedModelSettings,
     tokenizer: PreTrainedTokenizerBase,
 ) -> PreTrainedModel:
-    if model_settings.sequence_parallel_degree:
-        import turbo_alignment.modeling.parallel_states as parallel_states
-        from turbo_alignment.modeling.gemma2.patch import patch_gemma_attn_dict
-
-        patch_gemma_attn_dict()
-
-        parallel_states.initialize_model_parallel(sequence_parallel_size=model_settings.sequence_parallel_degree)
-        assert parallel_states.sequence_parallel_is_initialized()
-
     if model_settings.liger_kernels_settings is not None:
         apply_liger_kernel_to_llama(
             rope=model_settings.liger_kernels_settings.use_rope,
